afa79a7f0bb87e54d1605547cb8f6aebced901f7
paperec <91558365+paperec@users.noreply.github.com>
2024-02-22 12:36:02 +0100

    Propina personalizada, leccion 4

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,6 +19,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -68,7 +69,11 @@ fun TipTimeLayout() {
 
     var amountInput by remember { mutableStateOf("")}
     var amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+
+    var tipInput by remember { mutableStateOf("") }
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+
+    val tip = calculateTip(amount, tipPercent)
 
     Column(
         modifier = Modifier
@@ -85,11 +90,20 @@ fun TipTimeLayout() {
                 .align(alignment = Alignment.Start)
         )
         EditNumberField(
+            label = R.string.bill_amount,
             value = amountInput,
             onValueChange = { amountInput = it},
             modifier = Modifier
-            .padding(32.dp)
-            .fillMaxWidth()
+                .padding(32.dp)
+                .fillMaxWidth()
+        )
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            value = tipInput,
+            onValueChange = { tipInput = it },
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
         )
         Text(
             text = stringResource(R.string.tip_amount, tip),
@@ -101,6 +115,7 @@ fun TipTimeLayout() {
 
 @Composable
 fun EditNumberField(
+    @StringRes label: Int,
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
@@ -110,7 +125,7 @@ fun EditNumberField(
         onValueChange = onValueChange,
         singleLine = true,
         label = {Text(
-            stringResource(R.string.bill_amount)
+            stringResource(label)
         )},
         keyboardOptions = KeyboardOptions(
             keyboardType = KeyboardType.Number
